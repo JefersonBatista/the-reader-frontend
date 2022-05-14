cdd17399fee407ef63a824ad8004d99442911a4a
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-05-14 17:20:13 -0300

    Set API base URL to deployed back-end

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,12 +3,7 @@ import axios from "axios";
 import auth from "./auth";
 
 const axiosInstance = axios.create({
-  // There is no back-end deployed yet
-  // baseURL:
-  //   process.env.REACT_APP_ENV === "dev"
-  //     ? "http://localhost:5000"
-  //     : "http://localhost:5000",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://app-the-reader.herokuapp.com",
 });
 
 export { axiosInstance };
